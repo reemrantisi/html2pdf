faf5637f059d8a8e954ecd96621189fe367a7e33
Reem AlRantisi <reem@gmail.com>
2020-12-03 10:29:40 +0200

    uploaded to dropbox

--- html2pdf.py
+++ html2pdf.py
@@ -1,6 +1,7 @@
 import bs4
 import re
 from xhtml2pdf import pisa , parser
+import dropbox
 
 # Utility function
 def convert_html_to_pdf(source_html, output_filename):
@@ -17,6 +18,19 @@ def convert_html_to_pdf(source_html, output_filename):
 
     # return False on success and True on errors
     return pisa_status.err
+
+#transfer data to dropbox
+class TransferData:
+    def __init__(self, access_token):
+        self.access_token = access_token
+
+    def upload_file(self, output_filename, file_to):
+        """upload a file to Dropbox using API v2
+        """
+        dbx = dropbox.Dropbox(self.access_token)
+
+        with open(output_filename, 'rb') as f:
+            dbx.files_upload(f.read(), file_to)
 
 # Main program
 if __name__ == "__main__":
@@ -37,6 +51,12 @@ if __name__ == "__main__":
 
     pisa.showLogging()
     convert_html_to_pdf(source_html, output_filename)
+    
+    # upload to dropbox 
+    access_token = '3frTW7nRcYEAAAAAAAAAAebk3A4KAbarudYTc1DLEEUDJuYq-K4HEj2su-cNsA2q'
+    transferData = TransferData(access_token)
+    file_to = '/htmltopdf/test.pdf'  # The full path to upload the file to, including the file name
+    transferData.upload_file(output_filename, file_to)
 
 
 
